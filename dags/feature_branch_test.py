95fddb049a7499db33c519e2e2b82f066128937e
vermaaaditya038 <vermaaaditya038@gmail.com>
2025-05-05 00:56:26 +0530

    making a feature branch

--- dags/feature_branch_test.py
+++ dags/feature_branch_test.py
@@ -16,7 +16,7 @@ default_args = {
 
 # Defining the DAG
 dag = DAG(
-    'cicd_dag_run_actions',  # The name of the DAG
+    'feature_branch_test',  # The name of the DAG
     default_args=default_args,
     description='A simple DAG that prints current date',
     schedule_interval='@daily',  # Run once a day
